82a50c663f30f8dd7477bc2bfbb3371e0b052c09
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-09-28 11:09:06 -0300

    :sparkles: feat: implementado a rota get por id do heroi

--- DapperCrud/Controllers/SuperHeroController.cs
+++ DapperCrud/Controllers/SuperHeroController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace DapperCrud.Controllers
 {
@@ -7,5 +9,27 @@ namespace DapperCrud.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private readonly IConfiguration _config;
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.QueryAsync<SuperHero>("SELECT * FROM [Super-Heroes]");
+            return Ok(heroes);
+        }
+
+        [HttpGet("{heroId}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var hero = await connection.QueryFirstAsync<SuperHero>("SELECT * FROM [Super-Heroes] where id = @Id",
+                new { Id = heroId});
+            return Ok(hero);
+        }
     }
 }
--- DapperCrud/SuperHero.cs
+++ DapperCrud/SuperHero.cs
@@ -5,7 +5,8 @@
         public int Id { get; set; }
         public string NameHero { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
-        public string LatsName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
         public string Place { get; set; } = string.Empty;
     }
 }

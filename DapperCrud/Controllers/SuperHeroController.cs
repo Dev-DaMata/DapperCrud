a15176883dad3a551941222cf6252fa217613e5a
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-09-28 11:47:36 -0300

    :sparkles: feat: implementado a rota POST para adicionar herois

--- DapperCrud/Controllers/SuperHeroController.cs
+++ DapperCrud/Controllers/SuperHeroController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
 
 namespace DapperCrud.Controllers
 {
@@ -30,6 +31,19 @@ namespace DapperCrud.Controllers
             var hero = await connection.QueryFirstAsync<SuperHero>("SELECT * FROM [Super-Heroes] where id = @Id",
                 new { Id = heroId});
             return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateHero(SuperHero  hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into [Super-Heroes] (NameHero, FirstName, LastName, Place) values (@NameHero, @FirstName, @LastName, @Place)", hero);
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+        private static async Task<IEnumerable<SuperHero>> SelectAllHeroes(SqlConnection connection)
+        {
+            return await connection.QueryAsync<SuperHero>("select * from  [Super-Heroes]");
         }
     }
 }
